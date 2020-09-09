31c8924ee2e2ab401fc1275b7fcd32cf15ab9f23
JaswanthKomal10 <jaswanth1098@gmail.com>
2020-09-09 17:34:36 +0530

    Assignment 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
-import "./App.css";
+import DishDetail from "./DishdetailComponent";
 import { DISHES } from "./shared/dishes";
 
-class App extends Component {
+class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      selectedDish: null,
     };
   }
 
@@ -21,9 +22,10 @@ class App extends Component {
           </div>
         </Navbar>
         <Menu dishes={this.state.dishes} />
+        <DishDetail dish={} />
       </div>
     );
   }
 }
 
-export default App;
+export default Main;
